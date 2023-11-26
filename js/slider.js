6e9d605c98c5fc9e3cf4f84aedd9c5eb1368b306
Huden <klaud.i@mail.ru>
2023-11-26 17:58:22 +0300

    first commit

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+$(document).on('ready', function() {
+    $(".regular").slick({
+      dots: true,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 4500
+    });
+
+  });
