36216cdd3d07327c6608f6a7f3b5cd70d833980d
Huden <klaud.i@mail.ru>
2023-12-03 14:14:40 +0300

    added Turn off, on, b/w mode for pictures

--- js/version.js
+++ js/version.js
@@ -1,37 +1,36 @@
-const fontSizeButtons = document.querySelectorAll(".fontSizeButton");
-let previousFontSizes = {}; // To store previous font sizes
+let previousFontSizes = {};
 
-function toggleBlock() {
-    const block = document.getElementById("block");
-    block.classList.toggle("hidden");
+function toggleBlock(blockId) {
+    const block = document.getElementById(blockId);
 
-    if (block.classList.contains("hidden")) {
-        resetFontSizes();
-    } else {
-        removeAllStyles();
+    if (block) {
+        block.classList.toggle("hidden");
+        if (block.classList.contains("hidden")) {
+            resetFontSizes();
+            defaultButton.click();
+        } else {
+            removeAllStyles();
+        }
     }
 }
 
-fontSizeButtons.forEach(function (button) {
-    button.addEventListener("click", function () {
-        const fontSize = parseInt(button.dataset.fontSize);
-        const elementsToUpdate = document.querySelectorAll("h1, h2, a, p, span");
+function changeFontSize(fontSize) {
+    const elementsToUpdate = document.querySelectorAll("h1, h2, a, p, span");
 
-        elementsToUpdate.forEach(function (element) {
-            const tagName = element.tagName.toLowerCase();
+    elementsToUpdate.forEach(function (element) {
+        const tagName = element.tagName.toLowerCase();
 
-            if (!previousFontSizes[tagName]) {
-                previousFontSizes[tagName] = parseInt(window.getComputedStyle(element).fontSize);
-            }
+        if (!previousFontSizes[tagName]) {
+            previousFontSizes[tagName] = parseInt(window.getComputedStyle(element).fontSize);
+        }
 
-            const currentFontSize = parseInt(window.getComputedStyle(element).fontSize);
+        const currentFontSize = parseInt(window.getComputedStyle(element).fontSize);
 
-            if (currentFontSize !== previousFontSizes[tagName] + fontSize) {
-                element.style.fontSize = (previousFontSizes[tagName] + fontSize) + "px";
-            }
-        });
+        if (currentFontSize !== previousFontSizes[tagName] + fontSize) {
+            element.style.fontSize = (previousFontSizes[tagName] + fontSize) + "px";
+        }
     });
-});
+}
 
 function resetFontSizes() {
     const elementsToUpdate = document.querySelectorAll("h1, h2, a, p, span");
@@ -50,24 +49,49 @@ function resetFontSizes() {
     });
 }
 
-
-//change color on the website
-
 const defaultButton = document.getElementById("defaultButton");
 const blackButton = document.getElementById("blackButton");
 const blueButton = document.getElementById("blueButton");
 
 defaultButton.addEventListener("click", function() {
-  document.body.style.backgroundColor = "#ffffff";
-  document.body.style.color = "#000000";
+    document.body.style.backgroundColor = "#ffffff";
+    document.body.style.color = "#000000";
 });
 
 blackButton.addEventListener("click", function() {
-  document.body.style.backgroundColor = "#000000";
-  document.body.style.color = "#ffffff";
+    document.body.style.backgroundColor = "#000000";
+    document.body.style.color = "#ffffff";
 });
 
 blueButton.addEventListener("click", function() {
-  document.body.style.backgroundColor = "#00008b";
-  document.body.style.color = "#ff0";
+    document.body.style.backgroundColor = "#00008b";
+    document.body.style.color = "#ff0";
 });
+
+// Выключение, включение, режим ч/б
+
+function turnOff() {
+    var images = document.getElementsByTagName("img");
+    for (var i = 0; i < images.length; i++) {
+        images[i].style.display = "none";
+    }
+}
+
+function turnOn() {
+    var images = document.getElementsByTagName("img");
+    for (var i = 0; i < images.length; i++) {
+        images[i].style.display = "block";
+    }
+}
+
+function toggleColor() {
+    var images = document.getElementsByTagName("img");
+    for (var i = 0; i < images.length; i++) {
+        var image = images[i];
+        if (image.style.filter === "grayscale(100%)") {
+            image.style.filter = "none";
+        } else {
+            image.style.filter = "grayscale(100%)";
+        }
+    }
+}
